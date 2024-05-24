93c53e7d328e8b25c132b056af413475ffa5692f
jthop <2892650+jthop@users.noreply.github.com>
2024-05-24 11:25:32 -0500

    update

--- callbacks.py
+++ callbacks.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
+import argparse
 import json
 import logging
-import os
 from sys import argv
 
 if len(argv) <= 1:
@@ -23,10 +23,9 @@ if len(argv) <= 1:
     print("Note: Running with sudo might be needed for manual execution")
     exit()
 
-script_dir = os.path.dirname(os.path.abspath(argv[0]))
 
 logging.basicConfig(
-    filename=script_dir + "/callbacks.log",
+    filename="/callbacks.log",
     level=logging.DEBUG,
     format="%(asctime)s:%(name)s:%(levelname)s:%(message)s",
 )
@@ -34,61 +33,27 @@ logging.info("----------")
 logging.debug("Arguments %s", argv[1:])
 
 
-logging.info("Processing %s", argv[1])
-if argv[1] == "--list":
-    logging.debug("media,playlist")
+parser = argparse.ArgumentParser(description="RDS Setting Application")
+parser.add_argument(
+    "-t", "--type", help="Input station name (8 characters max)", required=False
+)
+parser.add_argument("-l", "--list", help="Song name", action="store_true")
+parser.add_argument("-d", "--data", help="Song name")
+args = parser.parse_args()
 
-elif argv[1] == "--reset":
-    # Not used by FPPD, but used by plugin_setup.php
-    logging.debug("RESET")
+if args.list:
+    # Tell the plugin that we should be registered for media
+    logging.debug(args.list)
 
-elif argv[1] == "--exit":
-    # Not used by FPPD, but useful for testing
-    logging.debug("EXIT")
+if args.type:
+    # Look for our type of plugin, doubt this is even needed at all
+    logging.debug(args.type)
+    # print "My type args: %s" %mytypearg
 
-elif argv[1] == "--type" and argv[2] == "media":
-    logging.info("Type media")
-
-    # TODO: Exception handling for json?
-    j = json.loads(argv[4])
-
-    # When default values are sent over fifo, other side more or less ignores them
-    media_type = j["type"] if "type" in j else "pause"
-    media_title = j["title"] if "title" in j else ""
-    media_artist = j["artist"] if "artist" in j else ""
-    media_tracknum = str(j["track"]) if "track" in j else "0"
-    media_length = str(j["length"]) if "length" in j else "0"
-
-    logging.debug("Type is %s", media_type)
-    logging.debug("Title is %s", media_title)
-    logging.debug("Artist is %s", media_artist)
-    logging.debug("Track # is %s", media_tracknum)
-    logging.debug("Length is %s", media_length)
-
-    if media_type == "pause" or media_type == "event":
-        # blank title, blank artist
-        pass
-    else:
-        # print(media_title)
-        # print(media_artist)
-        pass
-    # TODO: Will tracknum and length never show up at the wrong time?
-    # print(media_tracknum)
-    # print(media_length)
-
-
-elif argv[1] == "--type" and argv[2] == "playlist":
-    logging.info("Type playlist")
-
-    # TODO: Exception handling for json?
-    j = json.loads(argv[4])
-
-    playlist_action = j["Action"] if "Action" in j else "stop"
-
-    logging.info("Playlist action %s", j["Action"])
-
-    if playlist_action == "start":  # or playlist_action == 'playing':
-        pass
-    elif playlist_action == "stop":
-        pass
-logging.debug("Processing done")
+if args.data:
+    # get the json string from FPP
+    mydataarg = args.data
+    data = json.loads(mydataarg)
+    title = data["title"]
+    #   print "Title: %s" %title
+    logging.debug(args.data)
