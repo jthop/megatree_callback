68b6ef36b5ef4dd2a78ae6b920299616dc630036
jthop <2892650+jthop@users.noreply.github.com>
2024-11-01 00:44:04 -0500

    fix

--- callbacks.py
+++ callbacks.py
@@ -7,8 +7,6 @@ import os
 from sys import argv
 
 import requests
-from pydantic import BaseModel, ConfigDict
-from pydantic.alias_generators import to_camel
 
 URL = "https://api.megatr.ee/api/fpp/callback"
 DEV_MEDIA_URL = "http://10.10.2.5:8000/api/fpp/callback/media"
@@ -16,43 +14,6 @@ DEV_PLAYLIST_URL = "http://10.10.2.5:8000/api/fpp/callback/playlist"
 
 BEGIN_URL = "http://10.10.2.5:8000/api/fpp/callback/begin"
 END_URL = "http://10.10.2.5:8000/api/fpp/callback/end"
-
-
-class ParentModel(BaseModel):
-    model_config = ConfigDict(
-        from_attributes=True,
-        populate_by_name=True,
-        extra="ignore",
-        alias_generator=to_camel,
-    )
-
-
-class CurrentEntry(ParentModel):
-    duration: float | None = None
-    enabled: int
-    is_finished: int
-    is_playing: int
-    is_started: int
-    media_filename: str | None = None
-    media_name: str | None = None
-    playCount: int
-    play_once: int
-    timecode: str | None = None
-    type: str
-
-
-class PlaylistType(ParentModel):
-    Action: str
-    Item: int
-    Section: str
-    current_state: str
-    loop: int
-    loop_count: int
-    name: str
-    random: int
-    repeat: int
-    size: int
-    current_entry: CurrentEntry | None = None
 
 
 script_dir = os.path.dirname(os.path.abspath(argv[0]))
@@ -73,19 +34,20 @@ if args.list:
     print("media,playlist")
 
 if args.type:
-    data = PlaylistType.model_validate_json(args.data)
-    logging.debug("Wow")
+    data = json.loads(args.data)
     logging.debug(data)
 
     if args.type == "playlist":
         logging.info("playlist")
-        if data.Action == "query_next" and data.type == "media":
-            song = data.current_entry.media_filename
-            payload = {song: song}
-            r = requests.post(url=END_URL, json=payload)
+        if data.get("Action", "") == "query_next":
+            if data.get("currentEntry", {}).get("type", "") == "media":
+                song = data.get("currentEntry", {}).get("mediaFilename", "")
+                payload = {song: song}
+                r = requests.post(url=END_URL, json=payload)
 
-        elif data.Action == "playing" and data.type == "media":
-            song = data.current_entry.media_filename
-            duration = data.current_entry.duration
-            payload = {song: song, duration: duration}
-            r = requests.post(url=BEGIN_URL, json=payload)
+        elif data.get("Action", "") == "playing":
+            if data.get("currentEntry", {}).get("type", "") == "media":
+                song = data.get("currentEntry", {}).get("mediaFilename", "")
+                duration = data.get("currentEntry", {}).get("duration", "")
+                payload = {song: song, duration: duration}
+                r = requests.post(url=BEGIN_URL, json=payload)
--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -2,7 +2,7 @@
 set -x
 
 sudo apt-get update && sudo apt-get install -y python3 python3-pip;
-python3 -m pip install requests pydantic --break-system-packages;
+python3 -m pip install requests --break-system-packages;
 
 echo Restarting FPP...
 curl http://localhost/api/system/fppd/restart
