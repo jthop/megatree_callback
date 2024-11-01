828c95cdd2feaa91819882841ef5b2b808554bcd
jthop <2892650+jthop@users.noreply.github.com>
2024-11-01 00:22:12 -0500

    fix

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x
 
-sudo apt-get update && sudo apt-get install -y python3 python3-pip;
-python3 -m pip install requests pydantic;
+sudo apt-get update && sudo apt-get install -y python3 python3-pip python3-pydantic python3-requests;
 
 echo Restarting FPP...
 curl http://localhost/api/system/fppd/restart
