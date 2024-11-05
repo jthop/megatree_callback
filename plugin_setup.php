e97268b733a64e1184ab30f13db128ef88e5e8f1
jthop <2892650+jthop@users.noreply.github.com>
2024-11-05 17:18:11 -0600

    fix

--- plugin_setup.php
+++ plugin_setup.php
@@ -1,82 +1,128 @@
-
-
-
-
-
 <!-- last div intentionally skipped to fix footer background -->
-
-
-
-
 <?php
-
-
-$pluginName = basename(dirname(__FILE__));  //pjd 7-10-2019   added per dkulp 
-logEntry("plugin update file: ".$pluginUpdateFile);
-$DEBUG = false;
-
-$DEV_SERVER=trim($_POST["DEV_SERVER"]);
-$PROD_SERVER=trim($_POST["PROD_SERVER"]);
-$MODE=trim($_POST["MODE"]);
-
-if($DEBUG) {
-	echo "PORT: ".$_POST['PORT'];//print_r($_POST["PORT"]);
-	echo "loop message: ".$_POST["LOOPMESSAGE"]."<br/> \n";
+include_once "/opt/fpp/www/common.php"; //Alows use of FPP Functions
+$pluginName = basename(dirname(__FILE__));
+$pluginConfigFile = $settings['configDirectory'] ."/plugin." .$pluginName; //gets path to configuration files for plugin
+if (file_exists($pluginConfigFile)) {
+	$pluginSettings = parse_ini_file($pluginConfigFile);
 }
 
-if(isset($_POST['submit']))
-{
-
-//	echo "Writring config fie <br/> \n";
-
-	WriteSettingToFile("DEV_SERVER",$DEV_SERVER,$pluginName);
-	WriteSettingToFile("PROD_SERVER",$PROD_SERVER,$pluginName);
-	WriteSettingToFile("MODE",$MODE,$pluginName);
-} else {
-	$DEV_SERVER = $pluginSettings['DEV_SERVER'];
-	$PROD_SERVER = $pluginSettings['PROD_SERVER'];
-	$MODE = $pluginSettings['MODE'];
+if (strlen(urldecode($pluginSettings['DEV_SERVER']))<1){
+	WriteSettingToFile("DEV_SERVER","10.10.2.50",$pluginName);
+	$madeChange = true;
 }
-
-if(isset($_POST['updatePlugin']))
-{
-	logEntry("updating plugin...");
-	$updateResult = updatePluginFromGitHub($gitURL, $branch="master", $pluginName);
-
-	echo $updateResult."<br/> \n";
+if (strlen(urldecode($pluginSettings['DEV_SERVER']))<1){
+	WriteSettingToFile("PROD_SERVER","megatr.ee",$pluginName);
+	$madeChange = true;
+}
+if (strlen(urldecode($pluginSettings['DEV_SERVER']))<1){
+	WriteSettingToFile("MODE","DEV",$pluginName);
+	$madeChange = true;
+}
+if ($madeChange) {
+	$pluginSettings = parse_ini_file($pluginConfigFile);
 }
 
 ?>
 
+<!DOCTYPE html>
 <html>
 <head>
+
 </head>
+<body>
+<div class="pluginBody" style="margin-left: 1em;">
+	<div class="title">
+		<h1>megatr.ee</h1>
+		<h4></h4>
+	</div>
 
-<div id="megatree_callback" class="settings">
-    
-<fieldset>
-<legend>megatr.ee callback plugin</legend>
-
-<form method="post" action="http://<? echo $_SERVER['SERVER_ADDR'].":".$_SERVER['SERVER_PORT']?>/plugin.php?plugin=<?php echo $pluginName;?>&page=plugin_setup.php">
-
-    <?php 
-echo "ENABLE PLUGIN: ";
-
-if($ENABLED == "on" || $ENABLED == 1) {
-		echo "<input type=\"checkbox\" checked name=\"ENABLED\"> \n";
-} else {
-		echo "<input type=\"checkbox\"  name=\"ENABLED\"> \n";
-}
-echo "<p/> \n";
-
+<p>Press F1 for setup instructions</p>
+<table cellspacing="5">
+<tr>
+	<th style="text-align: left">Enable Show On Demand</th>
+<td>
+<?			
+	PrintSettingCheckbox("Show On Demand", "show_on_demand_enabled", $restart = 1, $reboot = 0, "true", "false", $pluginName = $pluginName, $callbackName = "", $defaultValue = 0, $desc = "", $sData = Array());
 ?>
-Dev Server: <input type="text" name="dev"><br>
-Prod Server: <input type="text" name="prod"><br>
-Mode: <input type="text" name="mode"><br>
+</td>
+</tr>
 
-<input type="submit">
-</form>
-</fieldset>
-</div>
-<br />
+
+<tr>
+	<th style="text-align: left">Voip.ms API Username</th>
+<td>
+<?
+//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
+	PrintSettingTextSaved("voipms_api_username", $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = $pluginName, $defaultValue = "");
+?>
+</td>
+</tr>
+
+<tr>
+	<th style="text-align: left">Voip.ms API Password</th>
+<td>
+<?
+//function PrintSettingPasswordSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "")
+	PrintSettingPasswordSaved("voipms_api_password", $restart = 1, $reboot = 0, $maxlength = 50, $size = 32, $pluginName = $pluginName, $defaultValue = "");
+?>
+</td>
+</tr>
+
+
+<tr>
+	<th style="text-align: left">Start Command</th>
+<td>
+<?
+//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
+	PrintSettingTextSaved("start_command", $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = $pluginName, $defaultValue = "");
+?>
+</td>
+</tr>
+
+<tr>
+	<th style="text-align: left">Success Message</th>
+<td>
+<?
+//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
+	PrintSettingTextSaved("message_success", $restart = 1, $reboot = 0, $maxlength = 160, $size = 100, $pluginName = $pluginName, $defaultValue = "");
+?>
+</td>
+</tr>
+
+
+<tr>
+	<th style="text-align: left">Not started message</th>
+<td>
+<?
+//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
+	PrintSettingTextSaved("message_not_started", $restart = 1, $reboot = 0, $maxlength = 160, $size = 100, $pluginName = $pluginName, $defaultValue = "");
+?>
+</td>
+</tr>
+
+<tr>
+	<th style="text-align: left">On-Demand Playlist</th>
+<td>
+<?
+	// function PrintSettingSelect($title, $setting, $restart = 1, $reboot = 0, $defaultValue, $values, $pluginName = "", $callbackName = "", $changedFunction = "", $sData = Array())
+	PrintSettingSelect("On-Demand Playlist", "on_demand_playlist", $restart = 1, $reboot = 0, "", $playlists, $pluginName);
+?>
+</td>
+</tr>
+
+<tr>
+	<th style="text-align: left">Show Playlist</th>
+<td>
+<?
+	PrintSettingSelect("Show Playlist", "main_playlist", $restart = 1, $reboot = 0, "", $playlists, $pluginName);
+?>
+</td>
+</tr>
+
+</table>
+
+
+
+</body>
 </html>
